448ff7089958a201bce1606d6ec242d0d41e74b9
Markus Dev <sanmarkusmorcoso@gmail.com>
2025-05-19 17:16:08 +0800

    Initial Deployment

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ import uploadRoutes from "./routes/fileUploadRoutes.js";
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://sdotayabas-ict-webapp.vercel.app/",
+    origin: "https://sdotayabas-ict-webapp.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
